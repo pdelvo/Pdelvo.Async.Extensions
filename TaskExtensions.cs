0f533697661c1980e258eaaa0b91e57f47b10e27
Dennis Fischer <fischer_dennis@live.de>
2012-08-05 12:05:27 +0200

    Added Socket.AcceptTaskAsync extension method

--- TaskExtensions.cs
+++ TaskExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace Pdelvo.Async.Extensions
 {
     public static class TaskExtensions
     {
+        public static Task<Socket> AcceptTaskAsync(this Socket owner)
+        {
+            return Task.Factory.FromAsync<Socket>(owner.BeginAccept, owner.EndAccept, null);
+        }
     }
 }
