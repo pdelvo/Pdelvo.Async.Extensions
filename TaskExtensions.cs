e704b289b1fd0ac48da92bf0c8b72df902891eb9
Dennis Fischer <fischer_dennis@live.de>
2012-08-08 23:46:17 +0200

    Added Socket.ConnectTaskAsync extension method

--- TaskExtensions.cs
+++ TaskExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,10 @@ namespace Pdelvo.Async.Extensions
         public static Task<Socket> AcceptTaskAsync(this Socket owner)
         {
             return Task.Factory.FromAsync<Socket>(owner.BeginAccept, owner.EndAccept, null);
+        }
+        public static Task ConnectTaskAsync(this Socket owner, IPEndPoint endPoint)
+        {
+            return Task.Factory.FromAsync<IPEndPoint>(owner.BeginConnect, owner.EndConnect, endPoint, null, TaskCreationOptions.None);
         }
     }
 }
